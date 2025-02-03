b0c83f7e939fa75fae3f7c2a62f055b66feb43ba
skotovskij <semyonkotovsky@Semyons-MacBook-Pro.local>
2025-02-03 16:36:24 +0300

    change ip

--- js/auth.js
+++ js/auth.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value.trim();
 
         try {
-            const response = await fetch(`/api/auth/login`, {
+            const response = await fetch(`${window.API_BASE_URL}/api/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
