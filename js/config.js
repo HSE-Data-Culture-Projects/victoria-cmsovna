6ad5898b13190bcfc63aad6b28aefa17eea41073
skotovskij <semyonkotovsky@Semyons-MacBook-Pro.local>
2025-02-03 16:46:31 +0300

    change paths

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 // js/config.js
 // Глобально определяем базовый URL для запросов к API
 window.API_BASE_URL = 'http://185.4.67.138:3000';
+console.log('config.js loaded, API_BASE_URL =', window.API_BASE_URL);
