a08ad4bce2ca4c3da28c311be3219f24ecb5cd90
skotovskij <semyonkotovsky@Semyons-MacBook-Pro.local>
2025-02-03 17:49:20 +0300

    fix

--- js/header_component.js
+++ js/header_component.js
@@ -5,13 +5,13 @@ class HeaderComponent extends HTMLElement {
                 <div class="edit-page-header">
                     <h2 style="padding-left: 16px; color: azure;">CMS Data Culture</h2>
                     <div class="edit-page-header">
-                        <button class="main-button" onclick="navigateTo('exams')">
+                        <button class="main-button" onclick="navigateTo('/exams')">
                             <img src="assets/svg/edit.svg" alt="Экзамены" class="icon"> Экзамены
                         </button>
-                        <button class="main-button" onclick="navigateTo('topics')">
+                        <button class="main-button" onclick="navigateTo('/topics')">
                             <img src="assets/svg/edit.svg" alt="Темы" class="icon"> Темы
                         </button>
-                        <button class="main-button" onclick="navigateTo('tasks')">
+                        <button class="main-button" onclick="navigateTo('/tasks')">
                             <img src="assets/svg/edit.svg" alt="Задания" class="icon"> Задания
                         </button>
                     </div>
@@ -20,3 +20,6 @@ class HeaderComponent extends HTMLElement {
         `;
     }
 }
+
+// Регистрируем кастомный элемент
+customElements.define('header-component', HeaderComponent);
